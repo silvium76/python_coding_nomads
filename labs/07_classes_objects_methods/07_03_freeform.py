b0acc44d66e5d60a5fdc7957f172cfd7984974b1
Silviu Marincas <marincas.silviu@gmail.com>
2020-08-27 16:37:49 +0300

    Update README.md

---
+++ labs/07_classes_objects_methods/07_03_freeform.py
@@ -0,0 +1,15 @@
+'''
+- Write a script with three classes that model everyday objects.
+- Each class should have an __init__ method that sets at least three attributes
+- Create at least two objects of each class using the __init__ method.
+- Include a __str__ method in each class that prints out the attributes
+    in a nicely formatted string.
+- Overload the __add__ method in one of the classes so that it's possible
+    to add attributes of two instances of that class using the + operator.
+- Once the objects are created, change some of the attribute values.
+
+Be creative. Have some fun. :)
+Using objects you can model anything you want.
+Cars, animals, poker games, sports teams, trees, beers, people etc...
+
+'''
